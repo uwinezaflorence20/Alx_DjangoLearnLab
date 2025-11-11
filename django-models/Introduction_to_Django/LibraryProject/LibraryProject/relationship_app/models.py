e9b188f8505ae5af719e2220c6969d54eb115ef3
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-11-11 09:47:15 +0200

    moved into right folder

--- django-models/Introduction_to_Django/LibraryProject/LibraryProject/relationship_app/models.py
+++ django-models/Introduction_to_Django/LibraryProject/LibraryProject/relationship_app/models.py
@@ -1,58 +1,35 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
-
-
+# Author Model
 class Author(models.Model):
-    """
-    Author model representing book authors.
-    Demonstrates the "one" side of a ForeignKey relationship.
-    """
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 
+# Book Model
 class Book(models.Model):
-    """
-    Book model with a ForeignKey relationship to Author.
-    Demonstrates Many-to-One relationship:
-    - Each book has one author
-    - An author can have multiple books
-    """
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
 
 
+# Library Model
 class Library(models.Model):
-    """
-    Library model with a ManyToMany relationship to Book.
-    Demonstrates Many-to-Many relationship:
-    - A library can have multiple books
-    - A book can be in multiple libraries
-    """
     name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book)
+    books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
 
 
+# Librarian Model
 class Librarian(models.Model):
-    """
-    Librarian model with a OneToOne relationship to Library.
-    Demonstrates One-to-One relationship:
-    - Each library has exactly one librarian
-    - Each librarian manages exactly one library
-    """
     name = models.CharField(max_length=200)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
 
     def __str__(self):
         return self.name
